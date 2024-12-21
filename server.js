27bc80566bcb18d9e44fac14aeee1a5f6f5e3175
Artemio Hdz Cst <artemiohernandecastillo@gmail.com>
2024-12-21 00:52:33 -0600

    add por string

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use((req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || '3000';
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
